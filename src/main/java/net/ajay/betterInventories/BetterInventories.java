e1d1c1b25d05d822e3715c2f9774eba6e4efbe8e
Ajay <87275549+4jayXD@users.noreply.github.com>
2025-12-28 11:53:08 +0000

    Initialize OperatorUtils in onEnable method
    Initialized OperatorUtils during plugin enable.

--- src/main/java/net/ajay/betterInventories/BetterInventories.java
+++ src/main/java/net/ajay/betterInventories/BetterInventories.java
@@ -2,14 +2,17 @@ package net.ajay.betterInventories;
 
 import net.ajay.betterInventories.manager.EChestManager;
 import net.ajay.betterInventories.manager.ShulkerBoxManager;
+import net.ajay.betterInventories.util.OperatorUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BetterInventories extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        OperatorUtils.init(this);
+        
         EChestManager.create(this);
-        ShulkerBoxManager.create(this); // shulker functionality disabled. As I dont know how shulkers work :(.. me smol brain.
+        ShulkerBoxManager.create(this);
     }
 
     @Override
